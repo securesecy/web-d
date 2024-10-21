5917057990fd63888bb4d21cb40ee54917d4fbff
Securesecy <krishnanjin109@gmail.com>
2024-10-21 14:33:50 +0530

    new

--- 21oct.js
+++ 21oct.js
@@ -1,3 +1,12 @@
 let str = "The Sky is blue"
 let ans= str.split(" ").reverse().join(" ");
 console.log(ans);
+
+let res= str.split(" ");
+let str1 = "";
+for(let i =0; i<res.length; i++){
+    str1 = str1+res[i].split("").reverse().join("")+" ";
+}
+console.log(str1);
+let ans2 = str.split(" ").reverse.join(" ");
+console.log(ans2);

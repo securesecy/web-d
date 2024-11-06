71cf8616a3f72e220d352a4333eeba8ce0ea76cb
securesecy <krishnanjin109@gmail.com>
2024-11-06 11:29:58 +0530

    math function

---
+++ mathFn.js
@@ -0,0 +1,25 @@
+let pi = 3.14;
+let PI=Math.PI;
+console.log(PI);
+
+
+
+console.log(Math.sqrt(64));
+console.log(Math.cbrt(64));
+
+
+let res = Math.min(2853,58743,475,99,215,4756,2544,45);
+console.log(res);
+let res1 = Math.max(7825,1458,7895,9874,2545,145,683,25,752,48);
+console.log(res1)
+
+let random = Math.random();
+console.log(random);
+
+
+console.log(Math.floor(4.999));
+console.log(Math.ceil(4.999));
+
+
+let randomValue = Math.floor(Math.random() * 6) + 50;  
+console.log(randomValue);
